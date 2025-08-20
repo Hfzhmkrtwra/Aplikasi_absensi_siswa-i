bed1116900fdd9b9bd187387f6845ff9260fd83b
burhan1308 <burhandoang59@gmail.com>
2025-08-20 10:42:21 +0700

    Membuat aplikasi ABSENSI SISWA sederhana menggunakan AI

---
+++ main.js
@@ -0,0 +1,31 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  getDoc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  query,
+  orderBy,
+  where
+} from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGYnq4VKq-YGu4RbfoI_ZHez9fishYjZo",
+  authDomain: "insan-cemerlang-afd2f.firebaseapp.com",
+  projectId: "insan-cemerlang-afd2f",
+  storageBucket: "insan-cemerlang-afd2f.appspot.com",
+  messagingSenderId: "686649580589",
+  appId: "1:686649580589:web:61374bbbd68adb604eaca4",
+  measurementId: "G-LNZTQBCE26"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Collection reference
+const absensiCollection = collection(db, "aplikasi_absensi_siswa");
